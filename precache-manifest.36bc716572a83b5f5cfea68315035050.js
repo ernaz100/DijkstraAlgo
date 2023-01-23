01863b1387613b9e95d1978ce4e9b5671e716aa7
Eric Nazarenus <nazarenusen@gmail.com>
2023-01-23 16:17:17 +0100

    Upload Project

---
+++ precache-manifest.36bc716572a83b5f5cfea68315035050.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fbff38340e316682756a0011ec9fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c5c2dd3ead73ba33a5",
+    "url": "/static/css/main.a7b50a49.chunk.css"
+  },
+  {
+    "revision": "e143e72e2a0cb83730de",
+    "url": "/static/js/2.d6481624.chunk.js"
+  },
+  {
+    "revision": "c6c5c2dd3ead73ba33a5",
+    "url": "/static/js/main.b42acf37.chunk.js"
+  },
+  {
+    "revision": "2e60a56cc9129176c7d8",
+    "url": "/static/js/runtime-main.5f132585.js"
+  }
+]);
